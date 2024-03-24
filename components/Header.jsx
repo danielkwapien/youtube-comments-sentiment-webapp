066913fa82c6b40ab81683e7f105d5105341fc41
Daniel Kwapien <danikwapien@gmail.com>
2024-03-24 16:58:13 +0100

    add dark mode

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,14 @@
 import Logo from '@components/Logo.jsx'
+import DarkModeToggle from './DarkModeToggle';
 
 const Navbar = () => {
  return(
     <header className='sticky top-0 z-50 bg-white dark:bg-gray-800'>
         <nav className='flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-800 max-w-7xl mx-auto'>
             <Logo />
+            <div className='flex-1 flex items-center justify-end space-x-4'>
+                <DarkModeToggle />
+            </div>
         </nav>
     </header>
  )
