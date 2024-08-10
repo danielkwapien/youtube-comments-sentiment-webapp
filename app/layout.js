5241d3c9b27e3220d2096ea0dba3f7b7d39ce874
Daniel Kwapien <danikwapien@gmail.com>
2024-08-10 17:58:04 +0200

    remove theme provider

--- app/layout.js
+++ app/layout.js
@@ -13,14 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem={true}
-          disableTransitionOnChange={true}
-        >
           {children}
-        </ThemeProvider>
       </body>
     </html>
   );
--- components/Header.jsx
+++ components/Header.jsx
@@ -4,11 +4,8 @@ import DarkModeToggle from './DarkModeToggle';
 const Navbar = () => {
  return(
     <header className='sticky top-0 z-50 bg-white dark:bg-black'>
-        <nav className='flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-black max-w-7xl mx-auto'>
+        <nav className='flex flex-col sm:flex-row items-start p-5 pl-2 bg-white dark:bg-black max-w-7xl mx-auto'>
             <Logo />
-            <div className='flex-1 flex items-center justify-end space-x-4'>
-                <DarkModeToggle />
-            </div>
         </nav>
     </header>
  )
